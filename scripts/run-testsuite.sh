90e24098932ad89f5d1ff7b0f3ef68823b5d181e
Matt Ho <matt.ho@gmail.com>
2024-08-10 12:57:34 -0700

    updated testsuite

--- scripts/run-testsuite.sh
+++ scripts/run-testsuite.sh
@@ -10,5 +10,8 @@ aws s3 cp s3://freemodel-artifacts-485660304150-us-west-2/scenarioql/main/latest
 chmod +x scenarioql
 
 ./scenarioql --version
+echo "./scenarioql --concurrency 12 --dir ${DIR} --endpoint ${ENDPOINT} --no-fail-on-error"
 ./scenarioql --concurrency 12 --dir "${DIR}" --endpoint "${ENDPOINT}" --no-fail-on-error
+
+find . -name
 
