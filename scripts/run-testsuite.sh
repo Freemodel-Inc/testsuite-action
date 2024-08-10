777e86eebb7eb78ceea42b02cd67b8bc2ad3144f
Matt Ho <matt.ho@gmail.com>
2024-08-10 12:31:22 -0700

    updated to automatically run tests

--- scripts/run-testsuite.sh
+++ scripts/run-testsuite.sh
@@ -4,6 +4,11 @@ set -eu
 
 echo running testsuite
 
-pwd
+# download and run the scenario tool
+#
+aws s3 cp s3://freemodel-artifacts-485660304150-us-west-2/scenarioql/main/latest/scenarioql .
+chmod +x scenarioql
+
+./scenarioql --help
 
 find .
